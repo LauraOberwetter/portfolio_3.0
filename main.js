11f7e8f3dfb489c9203f8b039023e0a607a25f1a
Laura Oberwetter <leoberwetter@gmail.com>
2021-05-23 18:00:16 -0500

    main nav done

---
+++ main.js
@@ -0,0 +1,16 @@
+// cursor
+// document.body.onmousemove = function(e) {
+//     document.documentElement.style.setProperty (
+//       '--x', (
+//         e.clientX+window.scrollX
+//       )
+//       + 'px'
+//     );
+//     document.documentElement.style.setProperty (
+//       '--y', (
+//         e.clientY+window.scrollY
+//       ) 
+//       + 'px'
+//     );
+//   }
+  
